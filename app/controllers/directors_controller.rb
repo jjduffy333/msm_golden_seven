ce46903432aafa465d1e79d08ff111a0970927dc
Jay D <jduffy@chicagobooth.edu>
2016-11-02 15:46:36 -0500

    jjduffy333_msmg7_directorsU

--- app/controllers/directors_controller.rb
+++ app/controllers/directors_controller.rb
@@ -11,9 +11,24 @@ class DirectorsController < ApplicationController
   def new_form
     @director = Director.new
   end
-  
+
   def create_row
     @director = Director.new
+    @director.dob = params[:dob]
+    @director.name = params[:name]
+    @director.bio = params[:bio]
+    @director.image_url = params[:image_url]
+    @director.save
+
+    render("show")
+  end
+
+  def edit_form
+    @director = Director.find(params[:id])
+  end
+
+  def update_row
+    @director = Director.find(params[:id])
     @director.dob = params[:dob]
     @director.name = params[:name]
     @director.bio = params[:bio]
--- config/routes.rb
+++ config/routes.rb
@@ -10,5 +10,11 @@ Rails.application.routes.draw do
   get('/directors', {:controller => 'directors', :action => 'index'})
   get('/directors/:id', {:controller => 'directors', :action => 'show'})
 
+  #Routes to UPDATE for DIRECTORS
+
+  get('/directors/:id/edit_form', {:controller => 'directors', :action => 'edit_form'})
+  get('/update_directors/:id', {:controller => 'directors', :action => 'update_row'})
+
+
 
 end
